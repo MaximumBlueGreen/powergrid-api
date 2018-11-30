61ad31e4f0596956cdfcee50b3520408fa749943
JohnWestwig <john.westwig@gmail.com>
2018-11-30 09:49:20 -0500

    routing example

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ const port = 3000
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}!`))
+
+var usersRouter = express.Router()
+
+usersRouter.get('/me', (req, res) => {
+  res.json({
+    handle: "John",
+    email: "john.westwig@gmail.com",
+    name: "John Westwig",
+  });
+});
+
+app.use('/users', usersRouter)
