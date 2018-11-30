34726c38dbf478eede043567e78c0462937096dd
JohnWestwig <john.westwig@gmail.com>
2018-11-30 10:19:19 -0500

    token stuff

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const tokenChecker = require('./token');
+const jwt = require('jsonwebtoken');
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}!`))
 
 var usersRouter = express.Router()
 
-usersRouter.get('/me', (req, res) => {
+usersRouter.get('/me', tokenChecker, (req, res) => {
   res.json({
     handle: "John",
     email: "john.westwig@gmail.com",
     name: "John Westwig",
+    user_id: req.user_id,
   });
 });
 
@@ -20,5 +24,14 @@ usersRouter.delete('/me', (req, res) => {
 	res.send(200);
 });
 
-app.use('/users', usersRouter)
+usersRouter.post('/me/authenticationToken', (req, res) => {
+  res.json({
+    token: jwt.sign({
+        id: 123
+    }, "SECRETSANTA", {
+        expiresIn: '24h'
+    })
+  })
+});
 
+app.use('/users', usersRouter)
