2988ae5a56ce7ee6fda24b5684d5c79823ee1e4f
JohnWestwig <john.westwig@gmail.com>
2019-01-30 14:23:21 -0500

    Use authorization in test

--- seeds/test.js
+++ seeds/test.js
@@ -3,7 +3,7 @@ exports.seed = function(knex) {
 	return knex("t_users").del()
 		.then(function () {
 			return knex("t_users").insert([
-				{ email: "foo@foo.com", handle: "foo", name: "Foo Goo"},
+				{ email: "foo@foo.com", handle: "foo", name: "Foo Goo" },
 			]);
 		});
 };
--- tests/users.js
+++ tests/users.js
@@ -5,9 +5,19 @@ if (process.env.NODE_ENV != "test") {
 	process.exit(1);
 }
 
+let token;
+
 beforeEach(() => knex.migrate.rollback()
 	.then(() => knex.migrate.latest())
 	.then(() => knex.seed.run())
+	.then(() =>
+		request(app)
+			.post("/users/me/authenticationToken")
+			.send({ email: "foo@foo.com" })
+			.then(res => {
+				token = res.body.token;
+			})
+	)
 );
 
 describe("GET /users/me", function () {
@@ -15,7 +25,7 @@ describe("GET /users/me", function () {
 		request(app)
 			.get("/users/me")
 			.set("Accept", "application/json")
-			.set("Authorization", "Bearer: foo")
-			.expect(400, done);
+			.set("Authorization", `Bearer ${token}`)
+			.expect(200, done);
 	});
 });
