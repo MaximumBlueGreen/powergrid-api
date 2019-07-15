f379bea38c4aeefd2d8676275c3a66bf8d5beccc
John Westwig <john.westwig@gmail.com>
2019-07-15 15:36:15 -0400

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,7 @@ const path = require("path");
 module.exports = {
 	client: "pg",
 	connection: process.env.DATABASE_URL,
+	ssl: true,
 	migrations: {
 		directory: path.normalize(path.join(__dirname, "/migrations")),
 		tableName: "knex_migrations",
