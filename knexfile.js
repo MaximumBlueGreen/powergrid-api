0ef946a0c4812963c805d90ff6cb6dc29069e185
John Westwig <john.westwig@nytimes.com>
2021-05-20 08:35:45 -0700

    ssl

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,10 @@ const path = require("path");
 
 module.exports = {
 	client: "pg",
-	connection: process.env.DATABASE_URL,
+	connection: `${process.env.DATABASE_URL}?ssl=true`,
 	migrations: {
 		directory: path.normalize(path.join(__dirname, "/migrations")),
 		tableName: "knex_migrations",
 	},
+	ssl: true,
 };
