0e35e4f2cc7f42137c3c7a4ddcbdda121077916c
John Westwig <john.westwig@nytimes.com>
2021-05-19 17:04:48 -0700

    try ssl

--- knexfile.js
+++ knexfile.js
@@ -3,10 +3,9 @@ const path = require("path");
 
 module.exports = {
 	client: "pg",
-	connection: process.env.DATABASE_URL,
+	connection: `${process.env.DATABASE_URL}?ssl=true`,
 	migrations: {
 		directory: path.normalize(path.join(__dirname, "/migrations")),
 		tableName: "knex_migrations",
 	},
-	ssl: true,
 };
